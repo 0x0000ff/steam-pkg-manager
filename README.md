a73ff99d156db39e7c9f1ffa883fa0ee8d064bf6
0x0000ff <0x0000ff@github.com>
2023-05-15 00:23:53 +0100

    almost there

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Preliminary Work on Package Management Tool(s) for Steamos
+This is designed to work with other package managers in order to generate a launcher on steam with artwork- and possibly bundle control schemes as well.
+
+Right now, it can run without root permissions. It will only support steam installations that have just one user.
+
+## TODO
+
+- ~~Add launcher data to steam~~
+- ~~Add artwork to launcher~~
+- Add controller mappings
+- Get it working with a package manager
+    - Make a service that runs the script when a new app is installled
+    - Get the service to startup at boot-time
+    - Add support for flatpak
+
+### Possible future developments
+- Add support for other package managers/formats
+- Support multiple users if there is a way to do it.
--- steam-launcher-create.py
+++ steam-launcher-create.py
@@ -1,56 +1,103 @@
 #!/usr/bin/python3
 import vdf
 import binascii
-import os
 import glob
+import os, sys
+from os.path import exists
+from stat import *
 parent_dir = "Steam Controller Configs"
+IS_STEAM_DIR = 0
+IS_FLAT_DIR = 1
 
-def getappid(data):
+def get_latest(dirLook, dirtype):
+    x = []
+    y = []
+    for f in os.scandir(dirLook):
+        if f.is_dir():
+            if (dirtype == IS_FLAT_DIR):
+               filepath=os.path.join(dirLook, f)
+            else:
+               filepath=os.path.join(dirLook, f)
+            if os.path.isdir(filepath):
+               y.append(os.path.getmtime(filepath))
+               y.append(filepath)
+               x.append(y)
+            y=[]
+    timefiles = sorted(x, reverse=True)
+    return timefiles[0][1]
+
+def get_appid(data):
    appid = binascii.crc32(data.encode('utf8')) | 0x80000000
    return appid
 
-def getbigappid(appid):
+def get_bigappid(appid):
    bigpic_appid = (appid <<32) | 0x02000000
 
-def makeControllerMappings(map_path, map_name):
+def make_controller_mappings(mappath, mapsourcepath):
    try:
-      os.makedirs(map_path)
+      os.makedirs(mappath)
    except FileExistsError:
-      print("controller map directory already exists")
-   try:
-      os.symlink(os.getcwd() + '/FXAA234076FA.vdf', str(map_path) + "/" +  str(map_name))
-   except FileExistsError:
-      print("controller map file already exists")
+      print("Controller map directory already exists.")
+   
+   for item in os.scandir(mapsourcepath):
+      try:
+         os.symlink(os.path.join(mapsourcepath, item.name), os.path.join(mappath, item.name))
+      except FileExistsError:
+         print("Controller map file already exists.")
 
-def makeAppArtwork(artwork_number, artwork_appid):
-      artwork_sections= ["logo", "grid", "hero", "icon"]
-      file_types=[".jpg", ".png", ".ico"]
+def make_app_artwork(artwork_number, artwork_appid, flatdir, steamdir):
+      trest = steamdir + "grid"
+      if not exists(steamdir + "grid") or not os.path.isdir(steamdir + 'grid'):
+         os.makedirs(steamdir + 'grid')
+
+      # p is designated to grids for some reason
+      artwork_sections= ["", "_logo", "p", "_hero"]
+      file_types=[".jpg", ".png"]
       for i in artwork_sections:
          for t in file_types:
-            if (glob.glob(i + '_' + str(artwork_number) + t)):
+            if (glob.glob(flatdir + str(artwork_number) + i + t)):
                try:
-                  os.symlink(i + "_" + str(artwork_number) + t,
-                           str(artwork_appid) + "_" + i + t)
+                  os.symlink(flatdir + str(artwork_number) + i + t,
+                           steamdir + "grid/" + str(artwork_appid) + i + t)
                except FileNotFoundError:
-                  print (i + "_" + str(artwork_number) + t + " not found")
+                  print (str(artwork_number) + i + t + " not found")
                except FileExistsError:
-                  print (i + "_" + str(artwork_number) + t + " already exits")
+                  print (str(artwork_number) + i + t + " already exits")
 
-if (os.path.isfile('shortcuts.vdf') == False):
-   open('shortcuts.vdf', mode='x')
+def main():
+   steamuserdir=get_latest(os.environ['HOME'] + '/.steam/steam/userdata/', IS_STEAM_DIR) + '/config/'
+   userid=os.path.basename(os.path.dirname(steamuserdir))
+   flatdir=get_latest(os.environ['HOME'] + '/.local/share/flatpak/app/', IS_FLAT_DIR) + '/current/active/files/share/steamlauncher/'
+   if not exists(flatdir):
+      print ("No launcher data directory found")
+      return
+   try:
+      t = vdf.load(open(flatdir + 'launcher.vdf'))
+   except FileNotFoundError:
+      print ("No launcher file found")
+      return
+   
+   if (os.path.isfile(steamuserdir + 'shortcuts.vdf') == False):
+      open(steamuserdir + 'shortcuts.vdf', mode='x')
+   d = vdf.binary_load(open(steamuserdir + 'shortcuts.vdf', mode='br'))
+   if ('shortcuts' not in d):
+      d = {'shortcuts': {}}
 
-t = vdf.load(open('placeholder.vdf'))
-d = vdf.binary_load(open('shortcuts.vdf', mode='br'))
+   for idx, item in enumerate(t.values()):
+      print (str(idx), '\n')
+      item['appid']=get_appid(''.join([item['Exe'], item['AppName']]))
+      if exists(flatdir+str(idx)+'_icon.ico'):
+         item['icon']=flatdir+str(idx)+'_icon.ico'
+      if exists(flatdir+str(idx)+'_icon.png'):
+         item['icon']=flatdir+str(idx)+'_icon.png'
+      d['shortcuts'][str(len(d['shortcuts']))]=item
+      print ("Created launcher" + " for " + item['AppName'])
+      make_app_artwork(idx, item['appid'], flatdir, steamuserdir)
+      print ("Applied launcher artwork" + " for " + item['AppName'])
+      map_path=os.environ['HOME'] + '/.steam/steam/steamapps/common/Steam Controller Configs/' + userid + '/config/' + item['AppName']
+      #make_controller_mappings(map_path, flatdir + '/controller_configs')
 
-if ('shortcuts' not in d):
-   d = {'shortcuts': {}}
+   b=vdf.binary_dump(d, open(steamuserdir + 'shortcuts.vdf', mode='bw'))
 
-for idx, item in enumerate(t.values()):
-   print (str(idx), '\n')
-   item['appid']=getappid(''.join([item['Exe'], item['AppName']]))
-   d['shortcuts'][str(len(d['shortcuts']))]=item
-   makeAppArtwork(idx, item['appid'])
-   map_path=os.path.join(parent_dir, str(item['appid']), "config", str(item['AppName']))
-   makeControllerMappings(map_path, item['appid'])
-
-b=vdf.binary_dump(d, open('shortcuts.vdf', mode='bw'))
+if __name__ == "__main__":
+   main();
